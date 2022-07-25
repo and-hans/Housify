80515a2600d2324bdf52b7133ca9999ba2e645e8
Andrew Hansraj <hansrajandrew@gmail.com>
2022-07-25 08:46:55 -0400

    added configurations

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,34 @@
 import datetime
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend/public", static_folder="../frontend/public")
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    """ This is a catch all that is required for react-router """
+    return render_template('index.html')
+
+# @app.route('/')
+# def index():
+#     return jsonify({'message':'hello'})
+
+
+@app.route('/mortgage', methods=['GET'])
+def mortgage():
+    if request.method == 'GET':
+        return jsonify(status=200, text="Here is a test response")
+
 
 @app.route('/time')
 def date_time():
     date_and_time = {'Name': 'Microsoft Experience', 'Age': 190, 'Date': datetime.datetime.now()}
     return jsonify(date_and_time)
 
+
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5001)
     app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
