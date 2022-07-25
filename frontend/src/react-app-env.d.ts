d895ea122208d98f3f5809d167d7fb27e1eeee6e
Andrew Hansraj <hansrajandrew@gmail.com>
2022-07-25 13:15:16 -0400

    modified ports and modules

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="react-scripts" />
 declare module 'react-plotly.js';
+declare module 'flowbite/plugin.js';
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,6 @@ module.exports = {
   },
   plugins: [
     require('@tailwindcss/forms'),
-    require('flowbite/plugin')
+    require('flowbite/plugin.js')
   ],
 }
