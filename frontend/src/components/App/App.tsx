c937d7974fbd9ba6381cd9b04dc03baf71ba0873
Andrew Hansraj <hansrajandrew@gmail.com>
2022-07-25 08:54:30 -0400

    added routes

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,18 +1,63 @@
 import React from 'react';
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
 import './App.css';
 import Navbar from '../Navbar/Navbar';
 import Mortgage from '../Mortgage/Mortgage';
 import Title from '../Title/Title';
 import MortgageBody from '../MortgageBody/MortgageBody';
+import Slideshow from '../Slideshow/Slideshow';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <div><Navbar /></div>
-      <div><Title prop={'Mortgage Calculator'}/></div>
-      <div><MortgageBody /></div>
-      <div><Mortgage /></div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/"
+        element={
+          <>
+            <div><Navbar/></div>
+            <div><Slideshow /></div>
+          </>
+        }
+        />
+        
+        <Route path="/mortgage"
+        element={
+          <>
+            <div><Navbar /></div>
+            <div><Title prop={'Mortgage Calculator'}/></div>
+            <div><MortgageBody /></div>
+            <div><Mortgage /></div>
+          </>
+        }
+        />
+
+        <Route path="/learn" element={
+          <>
+            <div><Navbar /></div>
+            <div><Title prop={'Learn'}/></div> 
+          </>
+        }
+        />
+
+        <Route path="/graphs" element={
+          <>
+            <div><Navbar /></div>
+            <div><Title prop={'Graphs'}/></div>  
+          </>
+        } 
+        />
+
+        <Route path="/about" element={
+          <>
+            <div><Navbar /></div>
+            <div><Title prop={'About'}/></div>  
+          </>
+        } 
+        />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
 
