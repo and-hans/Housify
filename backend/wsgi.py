bf1db5b3a28128e54484fc4605a60e1ba8eaf47d
Andrew Hansraj <hansrajandrew@gmail.com>
2022-07-27 13:35:45 -0400

    changed backend port

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,5 +2,5 @@ from app import app
 import os
 
 if __name__ == "__main__":
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5001)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5002)
     app.run(host='0.0.0.0', port=ENVIRONMENT_PORT)
